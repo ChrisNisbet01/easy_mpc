031920bfbe7937d6b432ea6a6af67494390735a4
Chris Nisbet <chris@nisbet.kiwi.nz>
2026-02-25 07:41:57 +1300

    Added support for a CPP comment parser.

--- include/easy_pc/easy_pc.h
+++ include/easy_pc/easy_pc.h
@@ -937,6 +937,26 @@ static inline epc_parser_t * epc_eoi_l(epc_parser_list * list, char const * name
 }
 
 /**
+ * @brief Creates a parser that matches a C++ style comment (// to end of line or EOF).
+ * @param name The name of the parser for debugging/CPT.
+ * @return A new `parser_t` instance, or NULL on error.
+ */
+EASY_PC_API epc_parser_t * epc_cpp_comment(char const * name);
+
+/**
+ * @brief Creates a parser that matches a C++ style comment (// to end of line or EOF).
+ *        This is a convenience wrapper for epc_cpp_comment that automatically adds the created
+ *        parser to the provided `epc_parser_list`.
+ * @param list The parser list to add to.
+ * @param name The name of the parser for debugging/CPT.
+ * @return A new `parser_t` instance, or NULL on error.
+ */
+static inline epc_parser_t * epc_cpp_comment_l(epc_parser_list * list, char const * name)
+{
+    return epc_parser_list_add(list, epc_cpp_comment(name));
+}
+
+/**
  * @brief Allocates and initializes a new parser object within the grammar's memory context.
  *
  * This function is typically used to create a forward reference to a parser that is needed to
